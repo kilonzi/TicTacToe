08653fc71b52af91ffd97e5429f2b30f5d7b3f63
Edemone <edem.agbenyo@gmail.com>
2019-10-28 16:52:03 +0000

    Implement user interface and game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,40 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+
+#Display welcome message and rules
+#Ask the player1 name 
+#Assign symbol to player 1
+#Ask player 2 name
+#Checking for same name, if same ask player 2 for another name
+#Assign symbol to player 2
+#Display an empty board
+#Ask users to press a key to start game
+#Asking the user for their move by position 1-9 
+#Prompt player 1 for move
+#Prompt player 2 for move
+#Display winner and name or draw
+
+PLAYER1_SIGN = "X"
+PLAYER2_SIGN = "O"
+puts "Welcome to the TIC-TAC TOE GAME. \nRules of the game: \n1.Each player has only move at a time. \n2. A player can put only assigned symbol. \nHOW TO WIN: \nThe player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner."
+puts "Please enter the name of the first player:"
+player1 = gets.chomp
+puts "#{player1}, you have been assigned #{PLAYER1_SIGN}"
+
+puts "Please enter the name of the second player:"
+player2 = gets.chomp
+puts "#{player2}, you have been assigned #{PLAYER2_SIGN}"
+
+puts "EMPTY BOARD"
+puts "Press Enter to start game"
+# start_key = get.chomp
+# while true
+	puts "Enter your move, any number within 1-9 and press Enter. \nEnter q to quit game."
+	choice = gets.chomp
+	if choice.to_i == 1..9
+		puts "Your choice is #{choice}"
+		
+	else
+		puts "You made an invalid choice."
+	end
+
+# end
