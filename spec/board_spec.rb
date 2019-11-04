d2f63ce9a56f4892c18fa79428082087c1926d9f
Edemone <edem.agbenyo@gmail.com>
2019-11-04 23:29:14 +0000

    Fix stickler issue

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "../lib/board"
+require_relative '../lib/board'
 describe Board do
   let(:board) { Board.new }
   describe 'game' do
     it 'returns status when given a sign' do
-      board.arr = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '] 
-      expect(board.status('X')).to be =='win'
+      board.arr = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+      expect(board.status('X')).to be == 'win'
     end
 
     it 'displays the board' do
-      board.arr = ['O', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' '] 
+      board.arr = ['O', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ']
       the_board = " O | X | X \n "
       the_board += "----------- \n"
       the_board += " X |   |   \n"
