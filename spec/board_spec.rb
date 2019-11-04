1c4c1a840007289aaae15830b3b369bc9936c77f
Edemone <edem.agbenyo@gmail.com>
2019-11-04 23:18:56 +0000

    Write test for board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,28 @@
+require_relative "../lib/board"
+describe Board do
+  let(:board) { Board.new}
+  describe "game" do
+    it "returns status when given a sign" do
+      board.arr=['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '] 
+      expect(board.status('X')).to be =='win'
+    end
+
+    it "displays the board" do
+      board.arr=['O', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' '] 
+      the_board = " O | X | X \n "
+      the_board += "----------- \n"
+      the_board += " X |   |   \n"
+      the_board += "----------- \n"
+      the_board += "   |   |   \n"
+      expect(board.display).to be == the_board
+    end
+  end
+
+  describe "game is win" do
+    it "when all marks are in a row" do
+      board.arr=['O', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O'] 
+      expect(board.win?('O')).to be_truthy
+    end
+  end
+
+end
