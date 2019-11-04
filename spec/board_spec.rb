f90d209dc3e789af1fda1c963160cdd03550985f
Edemone <edem.agbenyo@gmail.com>
2019-11-04 23:25:33 +0000

    Fix stickler issue

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require_relative "../lib/board"
 describe Board do
-  let(:board) { Board.new}
-  describe "game" do
-    it "returns status when given a sign" do
-      board.arr=['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '] 
+  let(:board) { Board.new }
+  describe 'game' do
+    it 'returns status when given a sign' do
+      board.arr = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '] 
       expect(board.status('X')).to be =='win'
     end
 
-    it "displays the board" do
-      board.arr=['O', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' '] 
+    it 'displays the board' do
+      board.arr = ['O', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' '] 
       the_board = " O | X | X \n "
       the_board += "----------- \n"
       the_board += " X |   |   \n"
@@ -18,11 +20,10 @@ describe Board do
     end
   end
 
-  describe "game is win" do
-    it "when all marks are in a row" do
-      board.arr=['O', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O'] 
+  describe 'game is win' do
+    it 'when all marks are in a row' do
+      board.arr = ['O', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O']
       expect(board.win?('O')).to be_truthy
     end
   end
-
 end
