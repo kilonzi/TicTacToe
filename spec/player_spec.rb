cda64e9c6708791be3202b1f8ae60c70b99fd563
Edemone <edem.agbenyo@gmail.com>
2019-11-05 16:29:05 +0000

    Add player test

---
+++ spec/player_spec.rb
@@ -0,0 +1,13 @@
+require_relative('../lib/player')
+
+describe Player do
+  let(:player){Player.new("Edem","O")}
+  describe "Player" do
+    it "name is returned" do
+      expect(player.name).to eq("Edem")
+    end
+    it "sign is returned" do
+      expect(player.sign).to eq("O")
+    end
+  end
+end
